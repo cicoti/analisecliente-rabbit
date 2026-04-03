98ed3dde7344172cec40bd6fd0a43ae72196f4e8
Silvio Cicoti <silvio.cicoti@gmail.com>
2026-04-03 16:55:53 -0300

    Update subir_tudo.bat

--- scripts/subir_tudo.bat
+++ scripts/subir_tudo.bat
@@ -1,6 +1,6 @@
 @echo off
-start "AppSolicitacaoAnaliseCliente" cmd /k "cd /d C:\projetos\ctech\analisecliente\rest\AppSolicitacaoAnaliseCliente && mvn spring-boot:run"
-start "AppVerificacaoCreditoCliente" cmd /k "cd /d C:\projetos\ctech\analisecliente\rest\AppVerificacaoCreditoCliente && mvn spring-boot:run"
-start "AppVerificacaoFichaLimpaCliente" cmd /k "cd /d C:\projetos\ctech\analisecliente\rest\AppVerificacaoFichaLimpaCliente && mvn spring-boot:run"
-start "AppVerificacaoStatusCliente" cmd /k "cd /d C:\projetos\ctech\analisecliente\rest\AppVerificacaoStatusCliente && mvn spring-boot:run"
-start "AppAgregadorAnaliseCliente" cmd /k "cd /d C:\projetos\ctech\analisecliente\rest\AppAgregadorAnaliseCliente && mvn spring-boot:run"
+start "AppSolicitacaoAnaliseCliente" cmd /k "cd /d C:\projetos\ctech\analisecliente\rabbitmq\AppSolicitacaoAnaliseCliente && mvn spring-boot:run"
+start "AppVerificacaoCreditoCliente" cmd /k "cd /d C:\projetos\ctech\analisecliente\rabbitmq\AppVerificacaoCreditoCliente && mvn spring-boot:run"
+start "AppVerificacaoFichaLimpaCliente" cmd /k "cd /d C:\projetos\ctech\analisecliente\rabbitmq\AppVerificacaoFichaLimpaCliente && mvn spring-boot:run"
+start "AppVerificacaoStatusCliente" cmd /k "cd /d C:\projetos\ctech\analisecliente\rabbitmq\AppVerificacaoStatusCliente && mvn spring-boot:run"
+start "AppAgregadorAnaliseCliente" cmd /k "cd /d C:\projetos\ctech\analisecliente\rabbitmq\AppAgregadorAnaliseCliente && mvn spring-boot:run"
