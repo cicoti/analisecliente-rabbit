21a0a1638d679050e0174f01bb2aa0f13dde9d38
Silvio Cicoti <silvio.cicoti@gmail.com>
2026-04-05 17:47:43 -0300

    Update AtualizarResultadoVerificacaoStatusClienteService.java

--- AppAgregadorAnaliseCliente/src/main/java/br/com/analisecliente/agregador/service/AtualizarResultadoVerificacaoStatusClienteService.java
+++ AppAgregadorAnaliseCliente/src/main/java/br/com/analisecliente/agregador/service/AtualizarResultadoVerificacaoStatusClienteService.java
@@ -4,47 +4,41 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import br.com.analisecliente.agregador.dto.ResultadoVerificacaoStatusClienteMessage;
 import br.com.analisecliente.agregador.model.ProcessoAnalise;
-import br.com.analisecliente.agregador.model.ResultadoAnalise;
-import br.com.analisecliente.agregador.repository.ProcessoAnaliseRepository;
 
 @Service
 public class AtualizarResultadoVerificacaoStatusClienteService {
 
-    private final ProcessoAnaliseRepository processoAnaliseRepository;
+    private final MongoTemplate mongoTemplate;
     private final ConsolidarResultadoAnaliseService consolidarResultadoAnaliseService;
 
     public AtualizarResultadoVerificacaoStatusClienteService(
-            ProcessoAnaliseRepository processoAnaliseRepository,
+            MongoTemplate mongoTemplate,
             ConsolidarResultadoAnaliseService consolidarResultadoAnaliseService) {
-        this.processoAnaliseRepository = processoAnaliseRepository;
+        this.mongoTemplate = mongoTemplate;
         this.consolidarResultadoAnaliseService = consolidarResultadoAnaliseService;
     }
 
     public void executar(ResultadoVerificacaoStatusClienteMessage message) {
-        ProcessoAnalise processoAnalise = processoAnaliseRepository.findById(message.getRequestId())
-                .orElseThrow(() -> new RuntimeException(
-                        "Processo não encontrado para requestId=" + message.getRequestId()));
-
-        ResultadoAnalise resultadoAnalise = processoAnalise.getResultado();
-        if (resultadoAnalise == null) {
-            resultadoAnalise = new ResultadoAnalise();
-            processoAnalise.setResultado(resultadoAnalise);
-        }
-
         Map<String, Object> verificacaoStatusCliente = new HashMap<String, Object>();
         verificacaoStatusCliente.put("clienteAtivo", message.getClienteAtivo());
         verificacaoStatusCliente.put("mensagem", message.getMensagem());
         verificacaoStatusCliente.put("dataHoraProcessamento", message.getDataHoraProcessamento());
 
-        resultadoAnalise.setVerificacaoStatusCliente(verificacaoStatusCliente);
+        Query query = Query.query(Criteria.where("_id").is(message.getRequestId()));
 
-        processoAnalise.setDataHoraAtualizacao(LocalDateTime.now());
+        Update update = new Update();
+        update.set("resultado.verificacaoStatusCliente", verificacaoStatusCliente);
+        update.set("dataHoraAtualizacao", LocalDateTime.now());
 
-        processoAnaliseRepository.save(processoAnalise);
+        mongoTemplate.updateFirst(query, update, ProcessoAnalise.class);
 
         System.out.println("Resultado de status do cliente gravado no Mongo. requestId=" + message.getRequestId());
 
